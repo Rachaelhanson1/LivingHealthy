9a9ae2c7f8aa76682f84f56933c7992f09f49fb1
Rachael Hanson <hansonr2@cardiff.ac.uk>
2022-03-09 13:29:09 +0000

    Initial Commit

---
+++ LivingHealthy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LivingHealthy
+//
+//  Created by Rachael Hanson on 09/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
