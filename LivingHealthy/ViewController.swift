b3292ec7a5fd4b05316f93cbe7146a9abf017537
Rachael Hanson <hansonr2@cardiff.ac.uk>
2022-03-16 19:23:15 +0000

    menu bar

--- LivingHealthy/ViewController.swift
+++ LivingHealthy/ViewController.swift
@@ -8,9 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var leading: NSLayoutConstraint!
+    @IBOutlet weak var trailing: NSLayoutConstraint!
+    
+    var menuOut = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func menutapped(_ sender: Any) {
+        if menuOut == false {
+            leading.constant = 150
+            trailing.constant = -150
+            menuOut = true
+        } else  {
+            leading.constant = 0
+            trailing.constant = 0
+            menuOut = false
+        }
+        
+    }
+    
+    
 }
